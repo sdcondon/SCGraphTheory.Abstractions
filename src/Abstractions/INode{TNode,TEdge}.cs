118cb7ae075315d23d1f8fa0eda55f1762f10604
Simon Condon <simoncondon@hotmail.com>
2021-04-11 15:46:28 +0100

    Constraint order change for consistency..
    .. just because it was bugging me :)

--- src/Abstractions/INode{TNode,TEdge}.cs
+++ src/Abstractions/INode{TNode,TEdge}.cs
@@ -8,8 +8,8 @@ namespace SCGraphTheory
     /// <typeparam name="TNode">The type of each node of the graph.</typeparam>
     /// <typeparam name="TEdge">The type of each edge of the graph.</typeparam>
     public interface INode<out TNode, out TEdge>
-        where TEdge : IEdge<TNode, TEdge>
         where TNode : INode<TNode, TEdge>
+        where TEdge : IEdge<TNode, TEdge>
     {
         /// <summary>
         /// Gets the collection of edges that are outbound from this node.
