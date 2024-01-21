a46853c036668cbeaf98f6cc466921bf9417027d
Simon Condon <simoncondon@hotmail.com>
2024-01-21 15:54:53 +0000

    Minor maintenance:
    * Updated obsolete GitHub workflow element
    * Switched to file-scoped namespaces

--- src/Abstractions/IEdge{TNode,TEdge}.cs
+++ src/Abstractions/IEdge{TNode,TEdge}.cs
@@ -1,22 +1,21 @@
-﻿namespace SCGraphTheory
+﻿namespace SCGraphTheory;
+
+/// <summary>
+/// Interface for types representing an edge in a <see cref="IGraph{TNode, TEdge}"/>.
+/// </summary>
+/// <typeparam name="TNode">The type of each node of the graph.</typeparam>
+/// <typeparam name="TEdge">The type of each edge of the graph.</typeparam>
+public interface IEdge<out TNode, out TEdge>
+    where TNode : INode<TNode, TEdge>
+    where TEdge : IEdge<TNode, TEdge>
 {
     /// <summary>
-    /// Interface for types representing an edge in a <see cref="IGraph{TNode, TEdge}"/>.
+    /// Gets the node that the edge connects from.
     /// </summary>
-    /// <typeparam name="TNode">The type of each node of the graph.</typeparam>
-    /// <typeparam name="TEdge">The type of each edge of the graph.</typeparam>
-    public interface IEdge<out TNode, out TEdge>
-        where TNode : INode<TNode, TEdge>
-        where TEdge : IEdge<TNode, TEdge>
-    {
-        /// <summary>
-        /// Gets the node that the edge connects from.
-        /// </summary>
-        TNode From { get; }
+    TNode From { get; }
 
-        /// <summary>
-        /// Gets the node that the edge connects to.
-        /// </summary>
-        TNode To { get; }
-    }
+    /// <summary>
+    /// Gets the node that the edge connects to.
+    /// </summary>
+    TNode To { get; }
 }
--- src/Abstractions/IGraph{TNode,TEdge}.cs
+++ src/Abstractions/IGraph{TNode,TEdge}.cs
@@ -1,24 +1,23 @@
 ﻿using System.Collections.Generic;
 
-namespace SCGraphTheory
+namespace SCGraphTheory;
+
+/// <summary>
+/// Interface for types representing a graph.
+/// </summary>
+/// <typeparam name="TNode">The type of each node of the graph.</typeparam>
+/// <typeparam name="TEdge">The type of each edge of the graph.</typeparam>
+public interface IGraph<out TNode, out TEdge>
+    where TNode : INode<TNode, TEdge>
+    where TEdge : IEdge<TNode, TEdge>
 {
     /// <summary>
-    /// Interface for types representing a graph.
+    /// Gets the set of nodes of the graph.
     /// </summary>
-    /// <typeparam name="TNode">The type of each node of the graph.</typeparam>
-    /// <typeparam name="TEdge">The type of each edge of the graph.</typeparam>
-    public interface IGraph<out TNode, out TEdge>
-        where TNode : INode<TNode, TEdge>
-        where TEdge : IEdge<TNode, TEdge>
-    {
-        /// <summary>
-        /// Gets the set of nodes of the graph.
-        /// </summary>
-        IEnumerable<TNode> Nodes { get; }
+    IEnumerable<TNode> Nodes { get; }
 
-        /// <summary>
-        /// Gets the set of edges of the graph.
-        /// </summary>
-        IEnumerable<TEdge> Edges { get; }
-    }
+    /// <summary>
+    /// Gets the set of edges of the graph.
+    /// </summary>
+    IEnumerable<TEdge> Edges { get; }
 }
